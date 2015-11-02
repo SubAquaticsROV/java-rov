046f53d7baac5e486d6750e1dba52d6a54810092
geemili <minergeemili@gmail.com>
2015-11-02 10:05:56 -0600

    Implemented basic echo program.
    - Added RXTX downloading to build script.

--- src/main/java/org/subaquatics/javarov/Main.java
+++ src/main/java/org/subaquatics/javarov/Main.java
@@ -1,12 +1,112 @@
 
 package org.subaquatics.javarov;
 
+import gnu.io.CommPort;
+import gnu.io.CommPortIdentifier;
+import gnu.io.SerialPort;
+
+import java.io.FileDescriptor;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 public class Main
 {
 
+	public void connect(String portName) throws Exception
+	{
+		CommPortIdentifier portIdentifier = CommPortIdentifier.getPortIdentifier(portName);
+		if(portIdentifier.isCurrentlyOwned())
+		{
+			System.out.println("Error: Port is currently in use");
+		}
+		else
+		{
+			CommPort commPort = portIdentifier.open(this.getClass().getName(),2000);
+			if (commPort instanceof SerialPort)
+			{
+				SerialPort serialPort = (SerialPort) commPort;
+				serialPort.setSerialPortParams(9600, SerialPort.DATABITS_8,SerialPort.STOPBITS_1,SerialPort.PARITY_NONE);
+
+				InputStream in = serialPort.getInputStream();
+				OutputStream out = serialPort.getOutputStream();
+
+				(new Thread(new SerialReader(in))).start();
+				(new Thread(new SerialWriter(out))).start();
+			}
+			else
+			{
+				System.out.println("Error: Only serial ports are handled by this example.");
+			}
+		}
+	}
+
+	public static class SerialReader implements Runnable
+	{
+		InputStream in;
+
+		public SerialReader(InputStream in)
+		{
+			this.in = in;
+		}
+
+		public void run()
+		{
+			byte[] buffer = new byte[1024];
+			int len = -1;
+			try
+			{
+				while( (len=this.in.read(buffer)) > -1 )
+				{
+					System.out.print(new String(buffer,0,len));
+				}
+			}
+			catch( IOException e )
+			{
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static class SerialWriter implements Runnable
+	{
+		OutputStream out;
+
+		public SerialWriter(OutputStream out)
+		{
+			this.out = out;
+		}
+
+		public void run()
+		{
+			byte[] buffer = new byte[1024];
+			int len = -1;
+			try
+			{
+				int c = 0;
+				while( (c=System.in.read()) > -1 )
+				{
+					this.out.write(0xF0);
+					this.out.write(c);
+				}
+			}
+			catch( IOException e )
+			{
+				e.printStackTrace();
+			}
+		}
+	}
+
 	public static void main(String[] args)
 	{
-		System.out.println("Hello, world!");
+		try
+		{
+			(new Main()).connect("COM5");
+		}
+		catch( Exception e )
+		{
+			e.printStackTrace();
+		}
 	}
 	
 }
