eb01b1ff1b28d87f107da73f7e3adb5e058550ce
geemili <minergeemili@gmail.com>
2016-01-30 16:54:03 -0600

    Added in kill channel to ROVListener

--- src/main/java/org/subaquatics/javarov/ROVListener.java
+++ src/main/java/org/subaquatics/javarov/ROVListener.java
@@ -1,6 +1,7 @@
 package org.subaquatics.javarov;
 
 import com.flipkart.lois.channel.api.SendChannel;
+import com.flipkart.lois.channel.api.ReceiveChannel;
 import com.flipkart.lois.channel.exceptions.ChannelClosedException;
 import java.io.InputStream;
 import java.io.IOException;
@@ -9,18 +10,26 @@ public class ROVListener implements Runnable {
 
 	InputStream input; // Where we listen to stuff from the ROV
 	SendChannel<String> messageOutput; // The place where we log the ROV's messages to
+	ReceiveChannel<Boolean> runningChannel; // Tells the thread when to close
 
-	public ROVListener(InputStream input, SendChannel<String> messageOutput) {
+	public ROVListener(InputStream input, SendChannel<String> messageOutput, ReceiveChannel<Boolean> runningChannel) {
 		this.input = input;
 		this.messageOutput = messageOutput;
+		this.runningChannel = runningChannel;
 	}
 
 	public void run() {
 		try {
 			byte[] buffer = new byte[1024];
 			int bytesRead = -1;
-			while ( (bytesRead = input.read(buffer)) > -1 ) {
+			boolean running = true;
+			while ( (bytesRead = input.read(buffer)) > -1 && running) {
 				messageOutput.send(new String(buffer, 0, bytesRead));
+
+				Boolean runningMessage = runningChannel.tryReceive();
+				if (runningMessage != null) {
+					running = runningMessage;
+				}
 			}
 		} catch(IOException e) {
 			e.printStackTrace();
