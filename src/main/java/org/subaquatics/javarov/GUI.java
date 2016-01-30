be9bfef2e0e8f5f7be4128cb75613836be9d34c4
geemili <minergeemili@gmail.com>
2016-01-30 14:21:14 -0600

    Added in MigLayout manager.

--- src/main/java/org/subaquatics/javarov/GUI.java
+++ src/main/java/org/subaquatics/javarov/GUI.java
@@ -2,18 +2,23 @@ package org.subaquatics.javarov;
 
 import java.awt.EventQueue;
 import javax.swing.*;
-
+import java.awt.BorderLayout;
+import net.miginfocom.swing.MigLayout;
 
 public class GUI extends JFrame implements Runnable {
 	private JTextField command;
 	private JTextArea outputarea;
 	
 	public GUI() {
-		command = new JTextField();
-		outputarea = new JTextArea();
-		add(command);
-		add(outputarea);
+		JPanel panel = new JPanel(new MigLayout());
+
+		outputarea = new JTextArea(25,30);
+		command = new JTextField(20);
+
+		panel.add(outputarea, "w 100%, wrap");
+		panel.add(command, "w 80%");
 		
+		add(panel, BorderLayout.SOUTH);
 	}
 
 	private void initUI() {
