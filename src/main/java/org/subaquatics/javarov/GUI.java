f9a202d6e661f83c376208ee4fe62465219318f0
autun12 <austindrane56@gmail.com>
2016-01-30 14:34:33 -0600

    add button

--- src/main/java/org/subaquatics/javarov/GUI.java
+++ src/main/java/org/subaquatics/javarov/GUI.java
@@ -8,15 +8,18 @@ import net.miginfocom.swing.MigLayout;
 public class GUI extends JFrame implements Runnable {
 	private JTextField command;
 	private JTextArea outputarea;
+	private JButton commandbutton;
 	
 	public GUI() {
 		JPanel panel = new JPanel(new MigLayout());
 
 		outputarea = new JTextArea(25,30);
 		command = new JTextField(20);
+		commandbutton = new JButton("Do Command");
 
 		panel.add(outputarea, "w 100%, wrap");
 		panel.add(command, "w 80%");
+		panel.add(commandbutton);
 		
 		add(panel, BorderLayout.SOUTH);
 	}
