44fc3ffa442e30e60e0bbbb80137793d83d76467
geemili <minergeemili@gmail.com>
2016-01-30 15:54:28 -0600

    Got demo working.

--- src/main/java/org/subaquatics/javarov/GUI.java
+++ src/main/java/org/subaquatics/javarov/GUI.java
@@ -6,14 +6,36 @@ import java.awt.BorderLayout;
 import net.miginfocom.swing.MigLayout;
 
 public class GUI extends JFrame implements Runnable {
+	private JLabel robotLabel;
+	private JComboBox robotComboBox;
+	private JButton robotButton;
+
+	private JLabel controllerLabel;
+	private JComboBox controllerComboBox;
+	private JButton controllerButton;
+
 	private JTextField command;
 	private JTextArea outputarea;
 	private JButton commandbutton;
 	
 	public GUI() {
-		JPanel panel = new JPanel(new MigLayout());
+		JPanel panel = new JPanel(new MigLayout("wrap 2"));
 
 		// COM ports
+		robotLabel = new JLabel("Robot: ");
+		robotComboBox = new JComboBox(new String[]{"Option one", "Option two"});
+		robotButton = new JButton("Connect");
+		robotButton.addActionListener((e) -> {
+			robotComboBox.setEnabled(!robotComboBox.isEnabled());
+		});
+
+		// Controller ports
+		controllerLabel = new JLabel("Controller: ");
+		controllerComboBox = new JComboBox(new String[]{"Option one", "Option two"});
+		controllerButton = new JButton("Start");
+		controllerButton.addActionListener((e) -> {
+			controllerComboBox.setEnabled(!controllerComboBox.isEnabled());
+		});
 
 		outputarea = new JTextArea(17, 1);
 		outputarea.setEditable(false);
@@ -28,10 +50,16 @@ public class GUI extends JFrame implements Runnable {
 		});
 
 		// Add stuff to the layout
-		panel.add(new JLabel("Robot"));
-		panel.add(new JLabel("Controller"), "wrap");
-		panel.add(new JScrollPane(outputarea), "grow, span 2, wrap");
-		panel.add(command, "w 80%");
+		panel.add(robotLabel, "split 3");
+		panel.add(robotComboBox);
+		panel.add(robotButton);
+
+		panel.add(controllerLabel, "split 3");
+		panel.add(controllerComboBox);
+		panel.add(controllerButton);
+
+		panel.add(new JScrollPane(outputarea), "grow, span 2");
+		panel.add(command, "growx");
 		panel.add(commandbutton);
 		
 		add(panel, BorderLayout.SOUTH);
@@ -39,7 +67,7 @@ public class GUI extends JFrame implements Runnable {
 
 	private void initUI() {
 		setTitle("GUI");
-		setSize(400, 400);
+		setSize(500, 400);
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
