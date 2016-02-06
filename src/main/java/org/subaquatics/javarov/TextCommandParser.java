983be08a25664a4f5f27ef3dd0e9bcdbb5178578
geemili <minergeemili@gmail.com>
2016-02-06 15:03:29 -0600

    Added set-pwmbounds

--- src/main/java/org/subaquatics/javarov/TextCommandParser.java
+++ src/main/java/org/subaquatics/javarov/TextCommandParser.java
@@ -1,14 +1,13 @@
 package org.subaquatics.javarov;
 
-import com.flipkart.lois.channel.api.SendChannel;
-import com.flipkart.lois.channel.exceptions.ChannelClosedException;
 import org.subaquatics.javarov.commands.*;
+import org.subaquatics.javarov.robots.Robot;
 
 public class TextCommandParser {
 
-	private SendChannel<Command> robot;
+	private Robot robot;
 
-	public TextCommandParser(SendChannel<Command> robot) {
+	public TextCommandParser(Robot robot) {
 		this.robot = robot;
 	}
 
@@ -16,57 +15,58 @@ public class TextCommandParser {
 	 * @return Returns a string describing the error or null.
 	 */
 	public String parse(String input) {
-		try {
-			String[] lines = input.split("\n");
-			for (String line: lines) {
-				String trimmed = line.trim();
-				if (trimmed.contains("#")) {
-					trimmed = trimmed.substring(0, trimmed.indexOf('#'));
-				}
-				String[] tokens = trimmed.split(" ");
-				if (tokens.length > 0) {
-					switch(tokens[0].toLowerCase()) {
-						case "set-motorpins":
+		String[] lines = input.split("\n");
+		for (String line: lines) {
+			String trimmed = line.trim();
+			if (trimmed.contains("#")) {
+				trimmed = trimmed.substring(0, trimmed.indexOf('#'));
+			}
+			String[] tokens = trimmed.split(" ");
+			if (tokens.length > 0) {
+				switch(tokens[0].toLowerCase()) {
+					case "set-motorpins":
+						break;
+					case "control-motor":
+						if (tokens.length < 4 || tokens.length > 4) {
+							return "Correct usage: control-motor <id> <direction> <speed>";
+						}
+						int motorId = Integer.parseInt(tokens[1]);
+						ControlMotorCommand.MotorDirection direction = ControlMotorCommand.MotorDirection.STOP;
+						switch (tokens[2].toLowerCase()) {
+						case "left":
+							direction = ControlMotorCommand.MotorDirection.LEFT;
 							break;
-						case "control-motor":
-							if (tokens.length < 4 || tokens.length > 4) {
-								return "Correct usage: control-motor <id> <direction> <speed>";
-							}
-							int motorId = Integer.parseInt(tokens[1]);
-							ControlMotorCommand.MotorDirection direction = ControlMotorCommand.MotorDirection.STOP;
-							switch (tokens[2].toLowerCase()) {
-							case "left":
-								direction = ControlMotorCommand.MotorDirection.LEFT;
-								break;
-							case "right":
-								direction = ControlMotorCommand.MotorDirection.RIGHT;
-								break;
-							case "stop":
-								direction = ControlMotorCommand.MotorDirection.STOP;
-								break;
-							}
-							int speed = Integer.parseInt(tokens[3]);
-							robot.trySend(new ControlMotorCommand(motorId, direction, speed));
+						case "right":
+							direction = ControlMotorCommand.MotorDirection.RIGHT;
 							break;
-						case "set-pwmbounds":
+						case "stop":
+							direction = ControlMotorCommand.MotorDirection.STOP;
 							break;
-						case "echo-string":
-							if (tokens.length < 2 || tokens.length > 2) {
-								return "\"echo-string\" takes only one argument!";
-							}
-							for (int i=0; i<tokens[1].length(); i++) {
-								byte c = (byte)(tokens[1].charAt(i));
-								robot.trySend(new EchoByteCommand(c));
-							}
-							break;
-						default:
-							return "Error: \""+tokens[0]+"\" is not a command.";
-					}
+						}
+						int speed = Integer.parseInt(tokens[3]);
+						robot.send(new ControlMotorCommand(motorId, direction, speed));
+						break;
+					case "set-pwmbounds":
+						if (tokens.length < 3 || tokens.length > 3) {
+							return "Correct usage: control-motor <min> <max>";
+						}
+						int min = Integer.parseInt(tokens[1]);
+						int max = Integer.parseInt(tokens[2]);
+						robot.send(new SetPWMBoundsCommand(min, max));
+						break;
+					case "echo-string":
+						if (tokens.length < 2 || tokens.length > 2) {
+							return "\"echo-string\" takes only one argument!";
+						}
+						for (int i=0; i<tokens[1].length(); i++) {
+							byte c = (byte)(tokens[1].charAt(i));
+							robot.send(new EchoByteCommand(c));
+						}
+						break;
+					default:
+						return "Error: \""+tokens[0]+"\" is not a command.";
 				}
 			}
-		} catch(ChannelClosedException e) {
-			e.printStackTrace();
-			return e.toString();
 		}
 		return null;
 	}
