c599a98f3efcda3324a17e91b3c8437ddfa605d4
geemili <minergeemili@gmail.com>
2016-02-06 14:51:09 -0600

    Got refreshing working.

--- src/main/java/org/subaquatics/javarov/SetupDialog.java
+++ src/main/java/org/subaquatics/javarov/SetupDialog.java
@@ -5,6 +5,8 @@ import javax.swing.*;
 import java.awt.BorderLayout;
 import java.util.ArrayList;
 import net.miginfocom.swing.MigLayout;
+import org.subaquatics.javarov.actions.RobotActions;
+import org.subaquatics.javarov.actions.ControllerActions;
 
 public class SetupDialog extends JFrame implements Runnable {
 
@@ -26,11 +28,21 @@ public class SetupDialog extends JFrame implements Runnable {
 		robotLabel = new JLabel("Robot: ");
 		robotComboBox = new JComboBox(new String[]{});
 		robotRefreshButton = new JButton("Refresh");
+		robotRefreshButton.addActionListener((e) -> {
+			RobotActions.getSerialPorts((ports) -> {
+				robotComboBox.setModel(new DefaultComboBoxModel(ports));
+			});
+		});
 
 		// Controller ports
 		controllerLabel = new JLabel("Controller: ");
 		controllerComboBox = new JComboBox(new String[]{});
 		controllerRefreshButton = new JButton("Refresh");
+		controllerRefreshButton.addActionListener((e) -> {
+			ControllerActions.getControllers((controllers) -> {
+				controllerComboBox.setModel(new DefaultComboBoxModel(controllers));
+			});
+		});
 
 		startButton = new JButton("Start");
 		startButton.addActionListener((e) -> {
